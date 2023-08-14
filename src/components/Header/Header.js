52672dd0b923d499a13e61cfca51770a5185d921
Andrew Senichkin <Kniws10@yandex.ru>
2023-08-14 17:58:15 +0300

    Верстка диплома light-3

---
+++ src/components/Header/Header.js
@@ -0,0 +1,45 @@
+import NavProfileBtn from "./NavProfileBtn/NavProfileBtn";
+import HeaderAuth from "./HeaderAuth/HeaderAuth";
+import Navigation from "./Navigation/Navigation";
+import HeaderNav from "./HeaderNav/HeaderNav";
+import Logo from "./Logo/Logo";
+import React from "react";
+import './Header.css'
+
+const Header = ({auth}) => {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    function handleClickOpen() {
+        setIsOpen(true)
+    }
+
+    function handleClickClose() {
+        setIsOpen(false)
+    }
+
+    return (
+    <div>
+        {auth ? (
+        <header className='header header__signup'>  
+            <div className='header__logo-container'>
+                <Logo/>
+                {auth && <HeaderNav onClick={handleClickOpen}/>}
+            </div>
+            {!auth ? <HeaderAuth/> : <NavProfileBtn isOpen={handleClickOpen}/>}
+            <Navigation isOpen={isOpen} onClick={handleClickClose}/>
+        </header>
+        ) : (
+            <header className='header'>  
+            <div className='header__logo-container'>
+                <Logo/>
+                {auth && <HeaderNav onClick={handleClickOpen}/>}
+            </div>
+            {!auth ? <HeaderAuth/> : <NavProfileBtn isOpen={handleClickOpen}/>}
+            <Navigation isOpen={isOpen} onClick={handleClickClose}/>
+        </header>  
+        )}
+    </div>
+    );
+}
+
+export default Header;
